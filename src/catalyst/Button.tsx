ed2a01299ce410d24d9eae54264ed2e3060ecdb4
Alex <alex@gogl.io>
2024-08-16 22:22:18 +0200

    fix icon button

--- src/catalyst/Button.tsx
+++ src/catalyst/Button.tsx
@@ -17,11 +17,6 @@ const button = tv({
     focusStyles,
     "disabled:opacity-50",
     "[&>svg]:-mx-0.5 [&>svg]:my-0.5 [&>svg]:h-5 [&>svg]:w-5 [&>svg]:shrink-0 sm:[&>svg]:my-1 sm:[&>svg]:h-4 sm:[&>svg]:w-4",
-    "before:absolute before:inset-0 before:-z-10 before:rounded-[calc(theme(borderRadius.lg)-1px)]",
-    "before:shadow",
-    "after:absolute after:inset-0 after:-z-10 after:rounded-[calc(theme(borderRadius.lg)-1px)]",
-    "after:shadow-[inset_0_1px_0_0_rgba(255,255,255,0.15)]",
-    "disabled:before:shadow-none disabled:after:shadow-none",
     "transition-colors duration-200 ease-in-out",
     "[&>svg]:transition-colors [&>svg]:duration-200 [&>svg]:ease-in-out",
     "before:transition-colors before:duration-200 before:ease-in-out",
@@ -38,6 +33,12 @@ const button = tv({
         "dark:before:bg-zinc-600 dark:hover:before:bg-zinc-500 dark:active:before:bg-zinc-400",
         "[&>svg]:text-zinc-300 hover:[&>svg]:text-zinc-200 active:[&>svg]:text-zinc-100",
         "dark:[&>svg]:text-zinc-300 dark:hover:[&>svg]:text-zinc-200 dark:active:[&>svg]:text-zinc-100",
+        "before:absolute before:inset-0 before:-z-10 before:rounded-[calc(theme(borderRadius.lg)-1px)]",
+        // shadow
+        "before:shadow",
+        "after:absolute after:inset-0 after:-z-10 after:rounded-[calc(theme(borderRadius.lg)-1px)]",
+        "after:shadow-[inset_0_1px_0_0_rgba(255,255,255,0.15)]",
+        "disabled:before:shadow-none disabled:after:shadow-none",
       ],
       secondary: [
         "text-zinc-950 bg-white border-zinc-950/10",
@@ -49,6 +50,12 @@ const button = tv({
         "dark:after:hover:bg-white/5 dark:after:active:bg-white/5",
         "[&>svg]:text-zinc-600 hover:[&>svg]:text-zinc-800 active:[&>svg]:text-zinc-900",
         "dark:[&>svg]:text-zinc-400 dark:hover:[&>svg]:text-zinc-300 dark:active:[&>svg]:text-zinc-200",
+        "before:absolute before:inset-0 before:-z-10 before:rounded-[calc(theme(borderRadius.lg)-1px)]",
+        // shadow
+        "before:shadow",
+        "after:absolute after:inset-0 after:-z-10 after:rounded-[calc(theme(borderRadius.lg)-1px)]",
+        "after:shadow-[inset_0_1px_0_0_rgba(255,255,255,0.15)]",
+        "disabled:before:shadow-none disabled:after:shadow-none",
       ],
       destructive: [
         "text-white bg-red-600 border-red-700/90",
@@ -56,6 +63,17 @@ const button = tv({
         "hover:before:bg-red-700 active:before:bg-red-800",
         "after:hover:bg-white/10 after:active:bg-white/10",
         "[&>svg]:text-red-200 hover:[&>svg]:text-red-100 active:[&>svg]:text-white",
+        "before:absolute before:inset-0 before:-z-10 before:rounded-[calc(theme(borderRadius.lg)-1px)]",
+        // shadow
+        "before:shadow",
+        "after:absolute after:inset-0 after:-z-10 after:rounded-[calc(theme(borderRadius.lg)-1px)]",
+        "after:shadow-[inset_0_1px_0_0_rgba(255,255,255,0.15)]",
+        "disabled:before:shadow-none disabled:after:shadow-none",
+      ],
+      icon: [
+        "p-1 bg-transparent border-transparent",
+        "[&>svg]:text-zinc-500 hover:[&>svg]:text-zinc-600 active:[&>svg]:text-zinc-700",
+        "dark:[&>svg]:text-zinc-400 dark:hover:[&>svg]:text-zinc-300 dark:active:[&>svg]:text-zinc-200",
       ],
     },
     isDisabled: {
@@ -76,6 +94,7 @@ export function Button({ children, ...props }: ButtonProps) {
       )}
     >
       <span
+        // Makes the touch area bigger
         className="absolute left-1/2 top-1/2 h-[max(100%,2.75rem)] w-[max(100%,2.75rem)] -translate-x-1/2 -translate-y-1/2 [@media(pointer:fine)]:hidden"
         aria-hidden="true"
       />
